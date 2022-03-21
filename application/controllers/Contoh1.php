9ec27108659c3b0847d43a23efcde23fba9e5224
hersandika03 <hersandika2121@gmail.com>
2022-03-21 08:38:49 +0700

    first commit

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public funstion index()
+    {
+    echo "<h1> Perkenalkan</h1>"; 
+    echo "nama Saya Hersandi NUrandhika
+    saya tinggal di rempoa
+    olahraga yang saya sukai tiduran";
+    } 
+}
