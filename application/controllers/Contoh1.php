b6f879110bd250f77db39a6c3f761017742310f7
hersandika03 <hersandika2121@gmail.com>
2022-03-28 12:20:21 +0700

    menambahkan tugas 2

--- application/controllers/Contoh1.php
+++ application/controllers/Contoh1.php
@@ -1,11 +1,11 @@
 <?php
 class Contoh1 extends CI_Controller
 {
-    public funstion index()
+    public function index()
     {
     echo "<h1> Perkenalkan</h1>"; 
-    echo "nama Saya Hersandi NUrandhika
-    saya tinggal di rempoa
-    olahraga yang saya sukai tiduran";
+    echo "Hai Nama Saya Hersandi Nurandhika,
+    Saya Tinggal Di Rumah,
+    Olahraga Yang Saya Sukai Badminton";
     } 
 }
